50c8831be64a673990299b182d63de508a56c7ca
unpatioli <unpatioli@gmail.com>
2017-01-24 18:33:41 +0300

    counts max queries

--- lib/time_q.rb
+++ lib/time_q.rb
@@ -1,7 +1,27 @@
 require "time_q/version"
+require "time_q/query"
 
 module TimeQ
   def self.parse(arr)
-    arr.map { |item| item.split("\t") }
+    arr.map do |item|
+      params = item.split("\t")
+      Query.new(params[0], params[1], params[2], params[3])
+    end
+  end
+
+  def self.max_queries(queue)
+    q_queue = parse(queue)
+    sorted_q = q_queue.sort { |a, b| a.finish <=> b.finish }
+
+    len = 0
+    last_finish = 0
+    sorted_q.each do |q|
+      if q.start >= last_finish
+        len += 1
+        last_finish = q.finish
+      end
+    end
+
+    len
   end
 end
--- spec/time_q_spec.rb
+++ spec/time_q_spec.rb
@@ -7,12 +7,27 @@ RSpec.describe TimeQ do
 
   context "parser" do
     before :each do
-      @arr = ["a\tb\tc", "d\te\tf"]
+      @arr = ["a\t1\t2\tya", "b\t3\t4\tvk"]
     end
 
     it 'parses array' do
       res = TimeQ::parse(@arr)
-      expect(res).to eq [["a", "b", "c"], ["d", "e", "f"]]
+      expect(res).to eq [TimeQ::Query.new('a', 1, 2, 'ya'), TimeQ::Query.new('b', 3, 4, 'vk')]
+    end
+  end
+
+  context 'counter' do
+    context 'counts queries to' do
+      it 'one API' do
+        queue = [
+          "A\t2\t5\tvk",
+          "B\t9\t7\tvk",
+          "C\t15\t6\tvk",
+          "D\t9\t3\tvk",
+        ]
+
+        expect(TimeQ.max_queries(queue)).to eq 3
+      end
     end
   end
 
